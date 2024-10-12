77da78515ac8ea7be027f7df26f965f4884a2cf7
Wayne Graham <wayne.graham@gmail.com>
2024-10-11 20:25:43 -0400

    Migrate to Astro-based theme

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Wayne Graham';
+export const SITE_DESCRIPTION =
+    "Explore my professional profile as a seasoned IT leader with expertise in informatics, cultural networks, and knowledge systems. Learn about my work, projects, and experiences";
+export const GENERATE_SLUG_FROM_TITLE = true
+
+export const TRANSITION_API = true
